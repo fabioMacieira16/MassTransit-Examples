b241d188a770b2f99a1e30e387e9e5b2ad324915
Fabio Macieira <fabiomacieira16@gmail.com>
2021-11-23 18:29:41 -0300

    Commit initial

---
+++ OrderService/OrderPublisher/TicketConsumer.cs
@@ -0,0 +1,25 @@
+﻿using MassTransit;
+using Microsoft.Extensions.Logging;
+using Shared.Model;
+using System;
+using System.Threading.Tasks;
+
+namespace OrderPublisher
+{
+    public class TicketConsumer : IConsumer<Ticket>
+    {
+        private readonly ILogger _logger;
+        public TicketConsumer(ILogger<TicketConsumer> logger)
+        {
+            this._logger = logger;
+        }
+
+        public async Task Consume(ConsumeContext<Ticket> context)
+        {
+            await Console.Out.WriteAsync(context.Message.UserName);
+
+            _logger.LogInformation($"NEW MESSAGE RECEIVED : " + 
+                $" { context.Message.UserName } { context.Message.Location }" );
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# MassTransit-Exemple
- 
+# Recursos ultilizados
+
+.Net Core SDK 5.0
+RabbitMQ 
+Masstransit
+Masstransit.RabbitMQ
+Masstransit.AspNetCore
+
+** Interfaces a recursos mais usados com Asp .Net Core: **
+ - IBusControl -> Usado para iniciar/para o bus.
+ - IBus -> Public/Envia Mensagem no bus iniciando uma nova comunicação.
+ - ISendEndpoint -> Envias as mensagem a parti das dependências do consumer.
+ - AddMassTransit -> Método ultilizado para registrar os tipos para outros components
+ - MassTrasitHostService -> Inicia/para automaticamente a aplicação
+
+ ** Filas **
+ 	- rabbitmq://localhost/orderTicketQueue
